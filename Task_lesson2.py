7e6234ab331799c46460e26dd8513391310a316c
ViRiver24 <vi.river@gmail.com>
2024-11-27 20:25:45 +0200

    Add files via upload

---
+++ Task_lesson2.py
@@ -0,0 +1,339 @@
+import torch.optim as optim
+import os
+import tempfile
+import torch
+from torch import nn
+
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Import matplotlib for visualization
+import matplotlib.pyplot as plt
+
+
+# Setup training data
+train_data = datasets.MNIST(
+    root="Mnistdata", 
+    train=True, 
+    download=False, 
+    transform=ToTensor(), 
+    target_transform=None 
+)
+
+# Setup testing data
+test_data = datasets.MNIST(
+    root="Mnistdata",
+    train=False, 
+    download=True,
+    transform=ToTensor()
+)
+
+# See first training sample
+image, label = train_data[0]
+#print (image, label)
+
+# How many samples are there? 
+#print (len(train_data.data), len(train_data.targets), len(test_data.data), len(test_data.targets))
+# See classes
+class_names = train_data.classes
+#print (class_names) 
+
+import matplotlib.pyplot as plt
+image, label = train_data[0]
+#print(f"Image shape: {image.shape}")
+#plt.imshow(image.squeeze()) # image shape is [1, 28, 28] (colour channels, height, width)
+#plt.title(label)
+#plt.show()
+
+#plt.imshow(image.squeeze(), cmap="gray")
+#plt.title(class_names[label])
+#plt.show()
+
+from torch.utils.data import DataLoader
+
+# Setup the batch size hyperparameter
+BATCH_SIZE = 32
+
+# Turn datasets into iterables (batches)
+train_dataloader = DataLoader(train_data, # dataset to turn into iterable
+    batch_size=BATCH_SIZE, # how many samples per batch? 
+    shuffle=True # shuffle data every epoch?
+)
+
+test_dataloader = DataLoader(test_data,
+    batch_size=BATCH_SIZE,
+    shuffle=False # don't necessarily have to shuffle the testing data
+)
+
+
+# Check out what's inside the training dataloader
+train_features_batch, train_labels_batch = next(iter(train_dataloader))
+train_features_batch.shape, train_labels_batch.shape
+
+# Show a sample
+torch.manual_seed(42)
+random_idx = torch.randint(0, len(train_features_batch), size=[1]).item()
+img, label = train_features_batch[random_idx], train_labels_batch[random_idx]
+plt.imshow(img.squeeze(), cmap="gray")
+plt.title(class_names[label])
+plt.axis("Off");
+#print(f"Image size: {img.shape}")
+#print(f"Label: {label}, label size: {label.shape}")
+
+# Create a flatten layer
+flatten_model = nn.Flatten() # all nn modules function as a model (can do a forward pass)
+
+# Get a single sample
+x = train_features_batch[0]
+
+# Flatten the sample
+output = flatten_model(x) # perform forward pass
+
+# Print out what happened
+#print(f"Shape before flattening: {x.shape} -> [color_channels, height, width]")
+#print(f"Shape after flattening: {output.shape} -> [color_channels, height*width]")
+
+# Try uncommenting below and see what happens
+#print(x)
+#print(output)
+
+from torch import nn
+class MNISTModelV0(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.layer_stack = nn.Sequential(
+            nn.Flatten(), # flatten inputs into single vector
+            nn.Linear(in_features=input_shape, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=output_shape),
+            nn.ReLU()
+        )
+    
+    def forward(self, x):
+        return self.layer_stack(x)
+
+torch.manual_seed(42)
+
+#Need to setup model with input parameters
+model_0 = MNISTModelV0(input_shape=784, # one for every pixel (28x28)
+    hidden_units=10, # how many units in the hidden layer
+    output_shape=len(class_names) # one for every class
+)
+model_0.to("cpu") # keep model on CPU to begin with     
+
+import requests
+from pathlib import Path 
+
+# Import accuracy metric
+from helper_functions import accuracy_fn # Note: could also use torchmetrics.Accuracy(task = 'multiclass', num_classes=len(class_names)).to(device)
+
+# Setup loss function and optimizer
+loss_fn = nn.CrossEntropyLoss() # this is also called "criterion"/"cost function" in some places
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
+
+from timeit import default_timer as timer 
+def print_train_time(start: float, end: float, device: torch.device = None):
+
+    total_time = end - start
+    print(f"Train time on {device}: {total_time:.3f} seconds")
+    return total_time
+
+# Import tqdm for progress bar
+from tqdm.auto import tqdm
+
+# Set the seed and start the timer
+torch.manual_seed(42)
+train_time_start_on_cpu = timer()
+
+# Set the number of epochs (we'll keep this small for faster training times)
+epochs = 3
+
+# Create training and testing loop
+for epoch in tqdm(range(epochs)):
+
+    print(f"Epoch: {epoch}\n-------")
+    ### Training
+    train_loss = 0
+    # Add a loop to loop through training batches
+    for batch, (X, y) in enumerate(train_dataloader):
+        model_0.train() 
+        # 1. Forward pass
+        y_pred = model_0(X)
+
+        # 2. Calculate loss (per batch)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss # accumulatively add up the loss per epoch 
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+        # Print out how many samples have been seen
+        if batch % 400 == 0:
+            print(f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples")
+
+    # Divide total train loss by length of train dataloader (average loss per batch per epoch)
+    train_loss /= len(train_dataloader)
+    
+    ### Testing
+    # Setup variables for accumulatively adding up loss and accuracy 
+    test_loss, test_acc = 0, 0 
+    model_0.eval()
+    with torch.inference_mode():
+        for X, y in test_dataloader:
+            # 1. Forward pass
+            test_pred = model_0(X)
+           
+            # 2. Calculate loss (accumulatively)
+            test_loss += loss_fn(test_pred, y) # accumulatively add up the loss per epoch
+
+            # 3. Calculate accuracy (preds need to be same as y_true)
+            test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+        
+        # Calculations on test metrics need to happen inside torch.inference_mode()
+        # Divide total test loss by length of test dataloader (per batch)
+        test_loss /= len(test_dataloader)
+
+        # Divide total accuracy by length of test dataloader (per batch)
+        test_acc /= len(test_dataloader)
+
+    ## Print out what's happening
+    print(f"\nTrain loss: {train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%\n")
+
+# Calculate training time      
+train_time_end_on_cpu = timer()
+total_train_time_model_0 = print_train_time(start=train_time_start_on_cpu, 
+                                           end=train_time_end_on_cpu,
+                                           device=str(next(model_0.parameters()).device))
+image, label = test_data[2]
+#print(f"Image shape: {image.shape}")
+#plt.imshow(image.squeeze()) # image shape is [1, 28, 28] (colour channels, height, width)
+#plt.title(label)
+#plt.show()
+
+# Import tqdm for progress bar
+from tqdm.auto import tqdm
+
+# 1. Make predictions with trained model
+y_preds = []
+model_0.eval()
+with torch.inference_mode():
+  for X, y in tqdm(test_dataloader, desc="Making predictions"):
+    # Send data and targets to target device
+    X, y = X.to("cpu"), y.to("cpu")
+    # Do the forward pass
+    y_logit = model_0(X)
+    # Turn predictions from logits -> prediction probabilities -> predictions labels
+    y_pred = torch.softmax(y_logit, dim=1).argmax(dim=1) # note: perform softmax on the "logits" dimension, not "batch" dimension (in this case we have a batch size of 32, so can perform on dim=1)
+    # Put predictions on CPU for evaluation
+    y_preds.append(y_pred.cpu())
+# Concatenate list of predictions into a tensor
+y_pred_tensor = torch.cat(y_preds)
+
+# See if torchmetrics exists, if not, install it
+try:
+    import torchmetrics, mlxtend
+    print(f"mlxtend version: {mlxtend.__version__}")
+    assert int(mlxtend.__version__.split(".")[1]) >= 19, "mlxtend verison should be 0.19.0 or higher"
+except:
+    #!pip install -q torchmetrics -U mlxtend # <- Note: If you're using Google Colab, this may require restarting the runtime
+    import torchmetrics, mlxtend
+    print(f"mlxtend version: {mlxtend.__version__}")
+
+# Import mlxtend upgraded version
+import mlxtend 
+#print(mlxtend.__version__)
+assert int(mlxtend.__version__.split(".")[1]) >= 19 # should be version 0.19.0 or higher
+
+from torchmetrics import ConfusionMatrix
+from mlxtend.plotting import plot_confusion_matrix
+
+# 2. Setup confusion matrix instance and compare predictions to targets
+confmat = ConfusionMatrix(num_classes=len(class_names), task='multiclass')
+confmat_tensor = confmat(preds=y_pred_tensor,
+                         target=test_data.targets)
+
+# 3. Plot the confusion matrix
+fig, ax = plot_confusion_matrix(
+    conf_mat=confmat_tensor.numpy(), # matplotlib likes working with NumPy 
+    class_names=class_names, # turn the row and column labels into class names
+    figsize=(10, 7)
+);
+plt.show()
+def make_predictions(model: torch.nn.Module, data: list, device: torch.device = "cpu"):
+    pred_probs = []
+    model.eval()
+    with torch.inference_mode():
+        for sample in data:
+            # Prepare sample
+            sample = torch.unsqueeze(sample, dim=0).to(device) # Add an extra dimension and send sample to device
+
+            # Forward pass (model outputs raw logit)
+            pred_logit = model(sample)
+
+            # Get prediction probability (logit -> prediction probability)
+            pred_prob = torch.softmax(pred_logit.squeeze(), dim=0) # note: perform softmax on the "logits" dimension, not "batch" dimension (in this case we have a batch size of 1, so can perform on dim=0)
+
+            # Get pred_prob off GPU for further calculations
+            pred_probs.append(pred_prob.cpu())
+            
+    # Stack the pred_probs to turn list into a tensor
+    return torch.stack(pred_probs)
+import random
+random.seed(42)
+test_samples = []
+test_labels = []
+for sample, label in random.sample(list(test_data), k=9):
+    test_samples.append(sample)
+    test_labels.append(label)
+
+# View the first test sample shape and label
+print(f"Test sample image shape: {test_samples[0].shape}\nTest sample label: {test_labels[0]} ({class_names[test_labels[0]]})")
+
+# Make predictions on test samples with model 0
+pred_probs= make_predictions(model=model_0, 
+                             data=test_samples)
+
+# View first two prediction probabilities list
+pred_probs[:2]
+# Turn the prediction probabilities into prediction labels by taking the argmax()
+pred_classes = pred_probs.argmax(dim=1)
+pred_classes
+
+# Are our predictions in the same form as our test labels? 
+test_labels, pred_classes
+
+# Plot predictions
+plt.figure(figsize=(9, 9))
+nrows = 3
+ncols = 3
+for i, sample in enumerate(test_samples):
+  # Create a subplot
+  plt.subplot(nrows, ncols, i+1)
+
+  # Plot the target image
+  plt.imshow(sample.squeeze(), cmap="gray")
+
+  # Find the prediction label (in text form, e.g. "Sandal")
+  pred_label = class_names[pred_classes[i]]
+
+  # Get the truth label (in text form, e.g. "T-shirt")
+  truth_label = class_names[test_labels[i]] 
+
+  # Create the title text of the plot
+  title_text = f"Pred: {pred_label} | Truth: {truth_label}"
+  
+  # Check for equality and change title colour accordingly
+  if pred_label == truth_label:
+      plt.title(title_text, fontsize=10, c="g") # green text if correct
+  else:
+      plt.title(title_text, fontsize=10, c="r") # red text if wrong
+  plt.axis(False);
+  plt.show()
+
+
